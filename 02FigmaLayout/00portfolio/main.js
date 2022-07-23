a22213f1227bd016442830c30fd7c1e1afef0fb8
script <slmsttrv1@gmail.com>
2022-07-23 11:19:43 +0500

    portfolio theme change js done

--- 02FigmaLayout/00portfolio/main.js
+++ 02FigmaLayout/00portfolio/main.js
@@ -4,33 +4,19 @@ let iconTheme = document.getElementById('themeIcon');
 let logo = document.getElementById('logoImg');
 let theme = 'light';
 
-function changeTheme(){
-    btnThemeChange.onclick = function(){
+btnThemeChange.addEventListener('click', () =>{
+    if(theme == 'light'){
         cssThemes.href = '../styledark.css';
         iconTheme.src = "/img/Moon-Dark.png";
         logo.src = 'img/Logo-Dark.png';
         theme = 'dark';
-    
-    if(theme == 'dark'){
-        btnThemeChange.onclick = function(){
-            cssThemes.href = '../style.css';
-            iconTheme.src = 'img/Moon.png';
-            logo.src = 'img/Logo.png';
-            theme = 'light'
-       
-        }
-         }else if(theme == 'light'){
-            btnThemeChange.onclick = function(){
-                cssThemes.href = '../styledark.css';
-                iconTheme.src = "/img/Moon-Dark.png";
-                theme = 'dark'
-            }
-         }
+    }else if(theme == 'dark'){
+        cssThemes.href = '../style.css';
+        iconTheme.src = 'img/Moon.png';
+        logo.src = 'img/Logo.png';
+        theme = 'light'
     }
-}
-
-changeTheme();
-
+})
 
 
 
