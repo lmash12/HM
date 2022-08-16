75e3ec3a0a3209614f958e3badc3ffcee8ecba2c
script <slmsttrv1@gmail.com>
2022-08-16 08:03:47 +0500

    add: dynamical title

--- 06JS/07DOM/dom.js
+++ 06JS/07DOM/dom.js
@@ -7,6 +7,7 @@ const goLang = document.querySelector(".go__container");
 const go = document.getElementById("goBoy");
 const headerTraffic = document.getElementById("trafficH");
 const containerDiv = document.querySelector(".container__div");
+const titleHTML = document.querySelector(".title");
 
 let counter = 0;
 
@@ -37,6 +38,7 @@ function changeBoxBg() {
         goLang.style.filter = "opacity(0)";
         headerTraffic.innerHTML = "Traffic Light";
         containerDiv.style.padding = "160px 20px"
+        titleHTML.innerHTML = "Red"
 
     } else if (counter == 4 || counter == 5 || counter == 6) {
         boxColor.style.background = "#F2C94C";
@@ -47,12 +49,14 @@ function changeBoxBg() {
         headerTraffic.innerHTML = "Traffic Light";
         go.style.width = "200px";
         containerDiv.style.padding = "160px 20px"
+        titleHTML.innerHTML = "Yellow"
     } else {
         boxColor.style.background = "#237A57";
         boxColor.style.borderRadius = "50%";
         boxColor.style.transition = "all 1s";
         warning.innerHTML = "Go!";
         goLang.style.filter = "opacity(1)";
+        titleHTML.innerHTML = "Green"
     }
 };
 
@@ -60,6 +64,7 @@ go.addEventListener("click", () => {
     headerTraffic.innerHTML = "You found a go-boy MALADES"
     go.style.width = "500px";
     containerDiv.style.padding = "50px 20px"
+    titleHTML.innerHTML = "Go Boy"
 })
 
 
